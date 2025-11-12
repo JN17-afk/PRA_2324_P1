a0f2d0c62b3b4efa684c12d68fb2e08f3d143d35
JNCASPER <jncasper@alumno.upv.es@ldsic-vdi20.upvnet.upv.es>
2025-11-12 18:00:49 +0100

    Cambios en el codigo de List.h

--- ListArray.h
+++ ListArray.h
@@ -1,13 +1,4 @@
-#include <ostream>
-#include "List.h"
 
-template <typename T> 
-class ListArray : public List<T> {
+	
 
-    private:
-        // miembros privados
-
-    public:
-        // miembros públicos, incluidos los heredados de List<T>
-    
-};
+  
